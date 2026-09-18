8a7c348a84cefe4ebe43f5110d49d40eb58bb2d6
saranmathesh7 <saransomu07@gmail.com>
2026-09-18 23:58:59 +0530

    Add Week 1 class problems

---
+++ src/control_statements/assigment_problems/NumberPyramid.java
@@ -0,0 +1,21 @@
+package control_statements.assigment_problems;
+import java.util.Scanner;
+public class NumberPyramid {
+    static void printNumberPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        printNumberPyramid(n);
+    }
+}
